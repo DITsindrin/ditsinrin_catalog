80cf9c8df5e7ec9b70c7921f8b85f675c56be4f8
ditsindrin <dmitriytsindrin1@gmail.com>
2023-11-19 20:00:09 +0300

    home_20.1.

--- online_store/views.py
+++ online_store/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 
+from online_store.models import Product, Contacts
+
+
 # Create your views here.
 
 
 def home(request):
+    products = Product.objects.order_by('-id')
+
+    print(products[:5])
     return render(request, 'online_store/home.html')
 
 
 def contacts(request):
+    contact = Contacts.objects.all()
+    print(contact)
+
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name}{phone}{message}')
-    return render(request, 'online_store/contacts.html')
+    return render(request, 'online_store/contacts.html', {'contact': contact})
