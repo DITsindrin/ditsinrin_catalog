f4364035153e3ceeb340d46bd5dfe403508704a6
ditsindrin <dmitriytsindrin1@gmail.com>
2023-11-26 21:18:49 +0300

    home_20.2.

--- online_store/urls.py
+++ online_store/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from online_store.apps import OnlineStoreConfig
 
-from .views import home, contacts
+from .views import home, contacts, products, product_detail, product_create, base_test
 
 app_name = OnlineStoreConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('products/', products, name='products'),
+    path('product-detail/<int:pk>', product_detail, name='product-detail'),
+    path('products-create/', product_create, name='product-create'),
+    path('base/', base_test, name='base'),
 ]
